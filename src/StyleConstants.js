87c5ab737511c22bcb6e77f03a5eee56754ac662
stujo <stujo@users.noreply.github.com>
2018-05-24 16:04:40 -0700

    Colors

--- src/StyleConstants.js
+++ src/StyleConstants.js
@@ -3,8 +3,10 @@ export default StyleConstants = {
     textInput: {
         borderColor: '#333333',
         borderWidth: 2,
+        color: '#333333',
     },
     invalidInput: {
         borderColor: '#ff3333',
+        color: '#ff3333',
     }
 };
